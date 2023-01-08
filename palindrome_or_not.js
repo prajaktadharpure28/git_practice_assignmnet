32b70ba7dc52ee5fe46436da5b143dab33402bdf
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-01-08 23:22:33 +0530

    correction in palindrome code

--- palindrome_or_not.js
+++ palindrome_or_not.js
@@ -1,10 +1,10 @@
-let bag="";
-for(let i=N-1;i>=0;i--){
-    bag=bag+str[i];
+let str = "madam";
+let rev = "";
+for(let i=str.length-1; i>=0; i--){
+    rev += str[i];
 }
-if(str==bag){
-    console.log("Yes");
-}
-else{
-    console.log("No");
+if(str === rev){
+    console.log("Palindrome");
+}else{
+    console.log("Not Palindrome");
 }
